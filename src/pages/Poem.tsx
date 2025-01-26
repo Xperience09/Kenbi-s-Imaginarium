2ff8bf8b99905ccc2ffe6f5db820cfcae7040a7e
Anubhav <anubhav.by@gmail.com>
2025-01-26 13:40:52 +0530

    Added railway call

--- src/pages/Poem.tsx
+++ src/pages/Poem.tsx
@@ -1,10 +1,29 @@
 import Cards from "../components/Cards";
-import { Poems } from "./poems/Poems";
+// import { Poems } from "./poems/Poems";
+import React, { useEffect, useState } from "react";
 
 const Poem = () => {
+  const [poems, setPoems] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://kenbiriba-service-core-production.up.railway.app/Poem/getAll",
+      {
+        method: "GET", // Ensure this matches the allowed methods
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => setPoems(data))
+      .catch((error) => console.error("Error fetching poems:", error));
+  }, []);
+
   return (
     <div className="page">
-      <Cards cardContent={Poems} />
+      <Cards cardContent={poems} />
     </div>
   );
 };
